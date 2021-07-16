66b6028974431737924c57ec55b55b2801fd14a9
VladaHryhorenko <mrsgriggy@gmail.com>
2021-07-16 11:45:12 +0300

    Added validation

--- js/Task2.js
+++ js/Task2.js
@@ -1,8 +1,25 @@
 //Написать функцию для подсчета количества букв в слове
 function countLetters(word) {
-    return word.length;
+    if (typeof word === 'string'){
+        return word.length;
+    } else {
+        console.log("This is not a string");
+    }
+    
 }
 
 let word = 'hello';
 let amountOfLetters = countLetters(word);
 console.log(amountOfLetters);
+
+let word2 = 123;
+console.log(countLetters(word2));
+
+let word3 = null;
+console.log(countLetters(word3));
+
+let word4;
+console.log(countLetters(word4));
+
+let word5 = false;
+console.log(countLetters(word5));
--- js/Task3.js
+++ js/Task3.js
@@ -1,11 +1,16 @@
 //Написать функцию для реверсии слова не используя встроенные методы
 
 function reverseVal(val) {
-    let res = '';
+    if (typeof val === 'string') {
+        let res = '';
     for (let i = val.length-1; i >= 0; i--) {
         res += val[i];
     }
     return res;
+    } else {
+        console.log("This is not a string");
+    }
+    
 }
 
 let value = 'hello';
@@ -15,3 +20,11 @@ console.log(reverseVal(value));
 let value2 = "А роза упала на лапу Азора";
 console.log(value2);
 console.log(reverseVal(value2));
+
+let value3 = 123;
+console.log(value3);
+console.log(reverseVal(value3));
+
+let value5 = null;
+console.log(value5);
+console.log(reverseVal(value5));
