0f3be9b4184278286de67454dbd84da982cf6d62
VladaHryhorenko <mrsgriggy@gmail.com>
2021-09-06 14:17:36 +0300

    Added changes to the task 2. Added the history of all operations

--- js/Task2.js
+++ js/Task2.js
@@ -1,16 +1,25 @@
 function cache() {
     let x = 0;
     let pow = 0;
-    let res = x**pow;
+    let res;
+    const arrHistory = [];
+    console.log(arrHistory);
     return function(a, b) {
-    if(x === a && pow === b) {
-        return {value: res, fromCache: true};
-    } else {
-        x = a;
-        pow = b;
-        res = x**pow;
-        return {value: res, fromCache: false};
-    }
+        let index = arrHistory.findIndex((el)=>(el.a === a && el.b === b));
+        switch(index) {
+            case -1:
+                x = a;
+                pow = b;
+                res = x**pow;
+                arrHistory.push({
+                    a: x,
+                    b: pow,
+                    res: res
+                })
+                return {value: res, fromCache: false};
+            default:
+                return {value: arrHistory[index].res, fromCache: true};
+        }
     }
 }
 
@@ -18,9 +27,14 @@ const calculate = cache();
 
 console.log(calculate(3, 3));// { value: 27, fromCache: false}
 console.log(calculate(2, 10));// { value: 1024, fromCache: false}
-console.log(calculate(2, 10));// { value: 1024, fromCache: true}
-console.log(calculate(3, 3));// { value: 1024, fromCache: false}
-console.log(calculate(2, 10));// { value: 1024, fromCache: false}
-console.log(calculate(2, 10));// { value: 1024, fromCache: true}
-console.log(calculate(3, 3));// { value: 1024, fromCache: false}
-console.log(calculate(3, 3));// { value: 1024, fromCache: true}
+// console.log(calculate(2, 10));// { value: 1024, fromCache: true}
+// console.log(calculate(3, 3));// { value: 1024, fromCache: false}
+// console.log(calculate(2, 10));// { value: 1024, fromCache: false}
+// console.log(calculate(2, 10));// { value: 1024, fromCache: true}
+// console.log(calculate(3, 3));// { value: 1024, fromCache: false}
+// console.log(calculate(3, 3));// { value: 1024, fromCache: true}
+console.log(calculate(5,5));
+console.log(calculate(3,3));
+console.log(calculate(3,4));
+console.log(calculate(3,4));
+
