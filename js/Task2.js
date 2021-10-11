cc07006dab8a0f21fe49b84f99111aff2c992d39
VladaHryhorenko <mrsgriggy@gmail.com>
2021-10-11 20:09:21 +0300

    Improved task 2

--- js/Task2.js
+++ js/Task2.js
@@ -1,70 +1,24 @@
-// const nestedList = ["Item", ["Item2", ["Item3"]]];
-// let root = document.body;
 
-
-// function createTree(arr, pos) {
-    
-//     const treeConstructor = (arr) => {
-//         let ul = document.createElement('ul'); 
-//         let li = document.createElement('li');
-//         arr.forEach(el => {
-//             if(typeof el === 'string') {
-//                 li.innerHTML = el;
-//                 ul.append(li);
-//                 console.log(1)
-//                 debugger;
-//             }
-//             else if (Array.isArray(el)) {
-//                 console.log(2);
-//                 return treeConstructor(el);
-//                 //ul.append(treeConstructor(el));
-//             } else {
-//                 throw new Error("Error! Type of the element is wrong")
-//             }
-//         });
-//     }
-//     pos.append(treeConstructor(arr));
-
-// }
-
-// createTree(nestedList, root); // передаем массив и куда добавлять список
-
-
-// arr.forEach(el => {
-    //     if(typeof el === 'string') {
-    //         li.innerHTML = el;
-    //         ul.append(li);
-    //     }
-    //     else if (Array.isArray(el)) {
-    //         return createTree(el, li);
-    //     } else {
-    //         throw new Error("Error! Type of the element is wrong")
-    //     }
-    // });
-
-
-const nestedList = ["Item", "Item2", ["Item3", ["Item4"]]];
+const nestedList = ["Item", "Item1", "Item2", ["Item3", "Item4", ["Item5"]]];
 let root = document.body;
 let mainUl = document.createElement('ul'); 
 root.append(mainUl);
 
 function createTree(arr, pos) {
     //let ul = document.createElement('ul'); 
-    let li = document.createElement('li');
+    let lastLi = document.createElement('li');
     arr.forEach(el => {
         if(typeof el === 'string') {
             //pos.append(ul);
-            
+
+            let li = document.createElement('li');
             li.innerHTML = el;
             pos.append(li);
-            
+            lastLi = li;
         }
         else if (Array.isArray(el)) {
-            pos=li;
-            console.log(li);
-            //debugger;
             let ul = document.createElement('ul'); 
-            pos.append(ul);
+            lastLi.append(ul);
             return createTree(el, ul);
         } else {
             throw new Error("Error! Type of the element is wrong")
